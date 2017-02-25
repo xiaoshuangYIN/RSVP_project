926431ce84c4e58523bbba0ca10770dbd9cfde52
Xiaoshuang Yin <Sharon@Xiaoshuangs-MacBook-Air.local>
2017-02-25 07:44:07 -0500

    add registration

--- RSVP_project/RSVP_project/urls.py
+++ RSVP_project/RSVP_project/urls.py
@@ -17,10 +17,13 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from login import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', views.register, name='register'), 
+    url(r'^userhome/$', views.user_home, name='userhome')
 ]
---
+++ RSVP_project/login/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User as User_Model
+from django import forms
+
+'''
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    # the info about the class
+    class Meta:
+        model = User_Model
+        fields = ('username', 'email', 'password')
+'''
+
+class UserForm(forms.Form):
+    User_name = forms.CharField(label = 'User name', max_length=100)
+    Email = forms.CharField(label = 'Email', max_length=100)
+    Password = forms.CharField(label = 'Password', max_length=100)
