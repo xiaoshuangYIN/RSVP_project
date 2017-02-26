f3f7ca906da3ba495354d2334517d83685732389
Xiaoshuang Yin <Sharon@Xiaoshuangs-MacBook-Air.local>
2017-02-26 07:16:33 -0500

    user can create event

--- RSVP_project/events/admin.py
+++ RSVP_project/events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
+from .models import Question
+from .models import Choice
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Question)
+admin.site.register(Choice)
--- RSVP_project/events/views.py
+++ RSVP_project/events/views.py
@@ -4,12 +4,34 @@ from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.auth.models import User
-
+from .forms import EventForm
+from .models import Event
 # Create your views here.
 def create_event(request):
+    form_class = EventForm
     template_name = 'CreateEvent.html'
-    return render(request, template_name)
-
+    
+    if request.method == 'GET':
+        form = form_class()
+        return render(request, template_name, {'event_form':form})
+    
+    if request.method == 'POST':
+        form = form_class(request.POST)
+        if form.is_valid():
+            # cleaned (normalized data)
+            event_name = form.cleaned_data['Event_name']
+            place = form.cleaned_data['Place']
+            start_time = form.cleaned_data['Start_time']
+            end_time = form.cleaned_data['End_time']
+            
+            # create a new user and save to database
+            user = Event.objects.create_event(event_name, place, start_time, end_time)
+            user.save()
+            return redirect('manageEventsIcreated')
+        #TO DO else:
+        else:
+            return redirect('userhome')
+    
 def manage_events(request):
     template_name = 'manageEventIcreated.html'
     return render(request, template_name)
